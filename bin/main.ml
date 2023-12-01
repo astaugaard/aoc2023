bb9579ae0b072236996c6e52d066f8f62ce30974
Aaron Staugaard <astaugaard@icloud.com>
2023-12-01 13:17:23 -0500

    added rest of days

--- bin/main.ml
+++ bin/main.ml
@@ -25,8 +25,56 @@ module Day (V : day)  = struct
 end
 
 module D1 = Day (Days.Day1)
+module D2 = Day (Days.Day2)
+module D3 = Day (Days.Day3)
+module D4 = Day (Days.Day4)
+module D5 = Day (Days.Day5)
+module D6 = Day (Days.Day6)
+module D7 = Day (Days.Day7)
+module D8 = Day (Days.Day8)
+module D9 = Day (Days.Day9)
+module D10 = Day (Days.Day10)
+module D11 = Day (Days.Day11)
+module D12 = Day (Days.Day12)
+module D13 = Day (Days.Day13)
+module D14 = Day (Days.Day14)
+module D15 = Day (Days.Day15)
+module D16 = Day (Days.Day16)
+module D17 = Day (Days.Day17)
+module D18 = Day (Days.Day18)
+module D19 = Day (Days.Day19)
+module D20 = Day (Days.Day20)
+module D21 = Day (Days.Day21)
+module D22 = Day (Days.Day22)
+module D23 = Day (Days.Day23)
+module D24 = Day (Days.Day24)
+module D25 = Day (Days.Day25)
 
-let days = [D1.runDay]
+let days = [D1.runDay;
+    D2.runDay;
+    D3.runDay;
+    D4.runDay;
+    D5.runDay;
+    D6.runDay;
+    D7.runDay;
+    D8.runDay;
+    D9.runDay;
+    D10.runDay;
+    D11.runDay;
+    D12.runDay;
+    D13.runDay;
+    D14.runDay;
+    D15.runDay;
+    D16.runDay;
+    D17.runDay;
+    D18.runDay;
+    D19.runDay;
+    D20.runDay;
+    D21.runDay;
+    D22.runDay;
+    D23.runDay;
+    D24.runDay;
+    D25.runDay]
 
 let run day verbose = 
   if day > 25 || day < 1 then print_endline "invalid day" 
