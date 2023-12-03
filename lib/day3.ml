b5c38706c13a5bec5307da8f48e7723916238a0e
astaugaard <64783657+astaugaard@users.noreply.github.com>
2023-12-03 10:17:15 -0500

    day 3 refactor

--- lib/day3.ml
+++ lib/day3.ml
@@ -3,90 +3,73 @@ open Utils
 open Angstrom
 open OUnit
 
-type input = char array array
+type input = char grid
 
 type showableInput = string list [@@deriving show]
 
-let show_input i = Iter.of_array i |> Iter.map (fun r -> Iter.of_array r |> Iter.to_str) |> Iter.to_list |> show_showableInput
+let show_input (Grid i) = Iter.of_array i |> Iter.map (fun r -> Iter.of_array r |> Iter.to_str) |> Iter.to_list |> show_showableInput
    
 let line = map (many1 (not_char '\n')) ~f:(fun l -> Iter.of_list l |> Iter.to_array)
  
-let parser = map (sep_by1 (char '\n') line) ~f:(fun l -> Iter.of_list l |> Iter.to_array)
-
+let parser = map (sep_by1 (char '\n') line) ~f:(fun l -> Grid(Iter.of_list l |> Iter.to_array))
 
 let get_opt l i = let len = Array.length l in 
   if i < len && i >= 0 then Some (Array.get l i) else None
 
 let index i (x,y) = Option.((get_opt i y) >>= (fun r -> get_opt r x) )
 
-let is_part (c : char option) = match c with
-  | None -> false
-  | Some a -> not (Char.is_digit a) && not Char.(a = '.')
+let is_part (c : char ) = not (Char.is_digit c) && not Char.(c = '.')
 
 let is_some_digit a = match a with
     | None -> false
     | Some a -> Char.is_digit a
 
-let get_loc_sum i (x,y) = let is_start = index i (x-1,y) |> is_some_digit |> not in
-  if is_start then 
-    (let num = Iter.iterate succ x |> Iter.take_while (fun lx -> index i (lx,y) |> is_some_digit) |> (* refactor later *)
-              Iter.map (fun lx -> Option.value_exn (index i (lx,y))) |> Iter.to_str in
-    let e = String.length num in
-    let near_part = Iter.int_range ~start:(-1) ~stop:1 |> 
-      Iter.flat_map (fun r -> Iter.int_range ~start:(-1) ~stop:e |> Iter.map (fun c -> (c,r))) |>
-      Iter.map (fun (dx,dy) -> is_part (index i (x+dx,y+dy))) |> 
-      Iter.fold (fun (a: bool) (b:bool) -> a || b) false 
-    in if near_part && index i (x,y) |> Option.value ~default:'.' |> Char.is_digit 
-       then int_of_string num else 0)
-  else 0
+let take_in_direction g (dx,dy) = 
+  Iter.iterate succ 0 |> Iter.take_while (fun l -> is_some_digit (Grid.getInDirection g (dx*l,dy*l))) |> 
+  Iter.map (fun j -> Option.value_exn (Grid.getInDirection g (dx*j,dy*j))) |> Iter.to_str 
+
+let move_and_take g dir = Option.((Grid.move g dir) >>= (fun l -> let s = take_in_direction l dir in if String.(s = "") then None else Some s))
+
+let singleton a = [a]
+
+let take_in_row g = 
+    if Grid.extract g |> Char.is_digit then 
+        let toRight = move_and_take g (1,0) in
+        let toLeft = take_in_direction g (-1,0) |> String.rev in
+        [toLeft ^ Option.value ~default:"" toRight |> int_of_string]
+    else let toRight = move_and_take g (1,0) |> Option.map ~f:(fun a -> int_of_string a |> singleton) in
+         let toLeft = move_and_take g (-1,0) |> Option.map ~f:(fun a -> String.rev a |> int_of_string |> singleton) in
+         Option.value ~default:[] toRight @ Option.value ~default:[] toLeft
 
 
+let numbersAround (g: char gridComonad) = 
+  let toLeft = move_and_take g (-1,0) |> Option.map ~f:(fun a -> String.rev a |> int_of_string |> singleton) in
+  let toRight = move_and_take g (1,0) |> Option.map ~f:(fun a -> int_of_string a |> singleton) in
+  let top_nums = Option.(Grid.move g (0,1) >>= (fun o -> Some (take_in_row o))) in
+  let bottom_nums = Option.(Grid.move g (0,-1) >>= (fun o -> Some (take_in_row o))) in
+  Iter.of_list [toLeft;toRight;top_nums;bottom_nums] |> 
+  Iter.flat_map (fun a -> Option.value ~default:[] a |> Iter.of_list) |>
+  Iter.to_list
+
+let get_loc_sum g = let loc = Grid.extract g in
+    if is_part loc then
+        let nums = numbersAround g
+        in List.fold ~f:(+) ~init:0 nums
+    else 0
 
 let partA (i : input): string = 
-  let h = Array.length i in 
-  let w = Array.length (Array.get i 0) in
-  Iter.int_range ~start:0 ~stop:(h-1) |> 
-  Iter.flat_map (fun r -> Iter.int_range ~start:0 ~stop:(w-1) |> Iter.map (fun c -> (c,r))) |>
-  Iter.map (fun l -> get_loc_sum i l) |>
-  Iter.sum |> string_of_int
+  Grid.extend_from_grid i get_loc_sum |> Grid.sum |> string_of_int
 
-let is_gear (c : char option) = match c with
-  | None -> false
-  | Some a -> Char.(a = '*')
+let is_gear (c : char) = Char.(c = '*')
 
+let get_loc_sumb g = let loc = Grid.extract g in
+    if is_gear loc then
+        let nums = numbersAround g in
+        if List.length nums = 2 then List.fold ~f:( * ) ~init:1 nums else 0
+    else 0
 
-let take_in_direction i (x,y) (dx,dy) = 
-  let ans = Iter.iterate succ 0 |> Iter.take_while (fun l -> is_some_digit (index i (x+dx*l,y+dy*l))) |> 
-            Iter.map (fun j -> Option.value_exn (index i (x+dx*j,y+dy*j))) |> Iter.to_str in
-  ans
-
-let take_in_row i (x,y) = if index i (x,y) |> is_some_digit then 
-        let toRight = take_in_direction i (x+1,y) (1,0) in
-        let toLeft = take_in_direction i (x,y) (-1,0) |> String.rev in
-        [toLeft ^ toRight]
-    else [take_in_direction i (x-1,y) (-1,0) |> String.rev;
-          take_in_direction i (x+1,y) (1,0)]
-
-let get_loc_sum2 i (x,y) =
-  if index i (x,y) |> is_gear then 
-    let get_left_num =  String.rev (take_in_direction i (x-1,y) (-1,0)) in
-    let get_right_num = take_in_direction i (x+1,y) (1,0) in
-    let bottom_nums = take_in_row i (x,y+1) in
-    let top_nums = take_in_row i (x,y-1) in
-    let all_nums = List.concat [[get_left_num];[get_right_num];bottom_nums;top_nums] |> Iter.of_list |>
-                   Iter.filter (fun l -> String.length l > 0) |> Iter.to_list in
-         if List.length all_nums = 2 then List.fold all_nums ~init:1 ~f:(fun a b -> a * (int_of_string b)) 
-         else 0
-  else 0
-
-
-let partB (i : input): string = 
-  let h = Array.length i in 
-  let w = Array.length (Array.get i 0) in
-  Iter.int_range ~start:0 ~stop:(h-1) |> 
-  Iter.flat_map (fun r -> Iter.int_range ~start:0 ~stop:(w-1) |> Iter.map (fun c -> (c,r))) |>
-  Iter.map (fun l -> get_loc_sum2 i l) |>
-  Iter.sum |> string_of_int
+let partB (i : input): string = Grid.extend_from_grid i get_loc_sumb |>
+                                Grid.sum |> string_of_int
 
 let tests = "tests" >::: [golden_test "day3" parser partA "4361" ~printer:show_input;
                           golden_test "day3" parser partB "467835" ~printer:show_input]
--- lib/utils.ml
+++ lib/utils.ml
@@ -1,6 +1,9 @@
 open Core
 open Angstrom
 
+
+exception UtilException of string
+
 let rec extend l f = match l with
   | [] -> []
   | (_ :: bs) -> f l :: extend bs f
@@ -26,22 +29,55 @@ let golden_test input parser ?printer part output : OUnit.test = TestCase (fun _
              let ans = part a in
              OUnit.assert_equal ~printer:(fun s -> Printf.sprintf "%s" s) output ans)
 
-type 'a gridComonad = GridComonad of 'a array array * int * int
+type 'a grid = Grid of 'a array array
 
-let extendG (GridComonad (l,x,y)) f = 
-  GridComonad (Array.mapi l 
-    ~f:(fun ri r -> Array.mapi r 
-           ~f:(fun ci _ -> f (GridComonad (l,ri,ci)))),x,y)
+type 'a gridComonad = GridComonad of 'a grid * int * int
 
-let focus (GridComonad (l,_,_)) (x,y) = GridComonad (l,x,y)
-
-let extract (GridComonad (l,x,y)) = 
-  let get_opt l i = let len = Array.length l in 
-                    if i < len && i >= 0 then Some (Array.get l i) 
-                    else None in
-
-  Option.((get_opt l y) >>= (fun r -> get_opt r x) ) 
-
+module Grid = struct
   
-let getInDirection (GridComonad (l,x,y)) (dx,dy) = 
-  focus (GridComonad (l,x,y)) (x+dx,y+dy) |> extract
+  let get_grid (GridComonad (g,_,_)) = g
+
+  let size (Grid g) = let h = Array.length g in 
+    if h > 0 then (h,Array.get g 0 |> Array.length) else (0,0)
+
+  let fold ~f ~init (Grid g)= 
+    Array.fold g ~init:init ~f:(fun acc r -> Array.fold r ~init:acc ~f:f)
+
+  let sum g = fold ~f:(+) ~init:0 g
+
+  let get_loc(Grid l) (x,y) = 
+    let get_opt l i = let len = Array.length l in 
+                      if i < len && i >= 0 then Some (Array.get l i) 
+                      else None in
+  
+    Option.((get_opt l y) >>= (fun r -> get_opt r x) ) 
+
+
+  let get_loc_exn g (x,y) = 
+    match get_loc g (x,y) with
+       | None -> raise (UtilException "no item at location")
+       | Some a -> a
+
+  let map_gridi (Grid l) f = Grid 
+     (Array.mapi l 
+        ~f:(fun ri r -> Array.mapi r 
+            ~f:(fun ci c -> f ci ri c)))
+  
+  let extend_from_grid g f = map_gridi g (fun c r _ -> f (GridComonad (g,c,r)))
+
+  let extend (GridComonad (g,x,y)) f = 
+    GridComonad (extend_from_grid g f,x,y)
+
+  let focus (GridComonad (l,_,_)) (x,y) = let (h,w) = size l in
+    if x < w && x >= 0 && y < h  && y >= 0 then Some (GridComonad (l,x,y)) else None
+
+  let move (GridComonad (l,x,y)) (dx,dy) = focus (GridComonad (l,x,y)) (dx+x,dy+y)
+
+  let extract (GridComonad (g,x,y)) = get_loc_exn g (x,y)
+
+  let getInDirection g (dx,dy) = 
+    match move g (dx,dy) with
+    | Some a -> Some (extract a)
+    | None -> None
+   
+end
