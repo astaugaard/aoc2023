21cb1f533e397448134ec0a5154f78400a84dd63
Aaron Staugaard <astaugaard@icloud.com>
2023-12-05 16:49:14 -0500

    day 5 part b

--- lib/day5.ml
+++ lib/day5.ml
@@ -31,6 +31,37 @@ let partA (Input (seeds,maps) : input): string =
   Iter.map (lookupSeed maps) |>
   Iter.min_exn |> string_of_int
 
-let partB (_ : input): string = "not yet implemented"
+let rec makeRanges l = match l with
+  | a::b::bs -> (a,b)::makeRanges bs
+  | _ -> []
 
-let tests = "tests" >::: [golden_test "day5" parser partA "35" ~printer:show_input;]
+type nrange = NRange of int * int
+
+let sortRanges = List.sort ~compare:(fun (a,_) (b,_) -> compare a b)
+
+let rec mapRanges map ranges = 
+  match (map,ranges) with
+  | (_,[]) -> []
+  | ([],a) -> a
+  | (((Range (d,s,a))::bs),((st,am)::rs)) ->
+       if (st + am) < s then (st,am)::mapRanges map rs (* completely before *)
+       else if (st+am) >= s && st < s then (st,s - st)::mapRanges map ((s,am + st - s)::rs) (* partialy before check if this is right*)
+       else if st >= s && (st + am) <= (s + a) then (st + d - s,am)::mapRanges map rs  (* covered *)
+       else if st >= s && st < (s + a) then (st + d - s,s+a - st)::mapRanges map ((s+a,st + am - s - a) :: rs)  (* partially in going out *) (* 45 19   64 6 *)
+       else if st >= (s+a) then mapRanges bs ranges else raise (UtilException "thing") (* after thing *)
+
+let lookupRanges maps seedRanges = List.fold_left 
+    ~f:(fun acc m -> let sortedMap = List.sort m ~compare:(fun (Range (_,a,_)) (Range (_,b,_)) -> compare a b) in 
+                     mapRanges sortedMap acc |> sortRanges)
+                        
+    ~init:(sortRanges seedRanges) maps
+
+let partB (Input (i,maps) : input): string = 
+  lookupRanges maps (makeRanges  i) |>
+  Iter.of_list |> 
+  Iter.map (fun (a,_) -> a) |>
+  Iter.min_exn |> string_of_int
+
+let tests = "tests" >::: [golden_test "day5" parser partA "35" ~printer:show_input; 
+                          golden_test "day5" parser partB "46" ~printer:show_input;
+                          final_answer_test 5 parser partA partB "836040384" "10834440"]
